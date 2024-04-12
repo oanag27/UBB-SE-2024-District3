8cbaf8a1c463974d24aae8c32cce486eab7fb193
diahl <herta.diana.5@gmail.com>
2024-04-12 19:16:59 +0300

    Full screen problems solved

--- Statistics/obj/Debug/net8.0-windows/Statistics.AssemblyInfo.cs
+++ Statistics/obj/Debug/net8.0-windows/Statistics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Statistics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9853e759d689bec1b946977bdd184b9f034b6b32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e6968cade41e9f8ecf3c734e0b100b9fe4b783")]
 [assembly: System.Reflection.AssemblyProductAttribute("Statistics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Statistics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
