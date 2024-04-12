24dec342553cbcd6033217f7eaf3abe9b83bff75
Delia-Gherasim <115105029+Delia-Gherasim@users.noreply.github.com>
2024-04-12 23:43:20 +0300

    merge with main

--- District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
+++ District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd8fdf16ee5c38d0aa75a166ef1bd0ab41803c08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1780c8b613f3a80c26eabfa002351d5387482f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
