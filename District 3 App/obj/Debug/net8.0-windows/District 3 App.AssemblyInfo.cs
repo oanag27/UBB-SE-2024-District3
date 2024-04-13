5787c39d3fe17b9d9a7549be224ffda47105c7c5
913-Groza-Iulia <iulia.maria.groza@stud.ubbcluj.ro>
2024-04-13 16:49:19 +0300

    login frontend done

--- District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
+++ District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+013d2500c55bd10ef7fcf9001ef2bf07bd398693")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cadc5f06e651762a0d3f90d49611d301dee619ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
