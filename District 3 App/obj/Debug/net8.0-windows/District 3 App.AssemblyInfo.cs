253260955603e9aad53a5c493b0b46b4f8a26d80
913-Goian-Sergiu <111054029+Sergiu2404@users.noreply.github.com>
2024-04-13 23:11:28 +0300

    aterizeaza profilul

--- District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
+++ District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b844c933612f5756fb21696c2da6af69056ec28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e6b622fc074877de73a78926b3fae17d30cbed")]
 [assembly: System.Reflection.AssemblyProductAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
