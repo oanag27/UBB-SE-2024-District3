7cb5c37d11d6ca60d838c64a9c5905c5ca0dbd29
inesEngel <ines.engel@stud.ubbcluj.ro>
2024-04-14 17:30:17 +0300

    payment confirmation looks prettier

--- District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
+++ District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f4f7dfd6caa750041c2584648d9edc01287338")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb2d1c7ede8b132db9fda3c0fdcdb43bfa0f93c")]
 [assembly: System.Reflection.AssemblyProductAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
