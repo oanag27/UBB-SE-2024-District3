e073fc037b95c07868c0d72e57c4b2f4402acc38
913-Gog-Oana <oana.gog@stud.ubbcluj.ro>
2024-04-15 20:48:57 +0300

    added buttons for posts

--- District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
+++ District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b167aaebcc7e441a2edd5d470bc977711289a322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e73af276aa4fcce9d004ac130bf7b0bcf1a64c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
