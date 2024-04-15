f614829f45555cc9e39daef2292c8d72bb5a2206
anitagorog <anitagorog2003@gmail.com>
2024-04-15 22:49:26 +0300

    friends

---
+++ District 3 App/FriendsWindow/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace District_3_App.FriendsWindow
+{
+    class Post
+    {
+        public int id { get; set; }
+
+        public Post(int Id) { id = Id; }
+    }
+}
