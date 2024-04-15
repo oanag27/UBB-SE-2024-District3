c6f0f2f28ce8a1aa28141500d016f42d815f9af1
PatriciaStoica <patricia.stoica@stud.ubbcluj.ro>
2024-04-15 10:48:48 +0300

    Profile Info and edit frontend

---
+++ District 3 App/ProfileInfo-GUI/ProfileInfoDisplay.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace District_3_App.ProfileInfo_GUI
+{
+    /// <summary>
+    /// Interaction logic for ProfileInfoDisplay.xaml
+    /// </summary>
+    public partial class ProfileInfoDisplay : UserControl
+    {
+        public ProfileInfoDisplay()
+        {
+            InitializeComponent();
+        }
+
+        private void EditInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            var newContent = new EditProfileInfo();
+
+            ProfileInfoGrid.Children.Clear();
+            ProfileInfoGrid.Children.Add(newContent);
+        }
+    }
+}
