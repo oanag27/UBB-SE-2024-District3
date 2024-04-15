9c82a35ee8bc9f64ee6c765c78a17ea6659c8238
anitagorog <161826051+anitagorog@users.noreply.github.com>
2024-04-15 14:38:57 +0300

    Add files via upload

---
+++ WpfApp2/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class Post
+    {
+        public int id { get; set; }
+
+        public Post(int Id) { id = Id; }
+    }
+}
