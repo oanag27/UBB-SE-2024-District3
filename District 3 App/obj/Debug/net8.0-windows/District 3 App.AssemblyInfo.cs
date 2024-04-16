741b37d817c6456feb1e59925556b1601a2c2de7
inesEngel <ines.engel@stud.ubbcluj.ro>
2024-04-16 16:14:32 +0300

    settings looks prettier

--- District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
+++ District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a3807f348e50bf1f6a9af140fd24e58e9d10f5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d934f697f4f80d4b6a54c0c7f23d37cabcb8f60")]
 [assembly: System.Reflection.AssemblyProductAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
