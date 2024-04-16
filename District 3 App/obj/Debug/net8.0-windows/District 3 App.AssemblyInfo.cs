305139e289a643b41a873d7475f885bd6dc78d5b
inesEngel <ines.engel@stud.ubbcluj.ro>
2024-04-16 21:04:36 +0300

    gets data from xml

--- District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
+++ District 3 App/obj/Debug/net8.0-windows/District 3 App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b20109290a40ea966356b6694b00cf11ea7135cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d66c8a385e92ddd1ba934794caeb8cba4362c99")]
 [assembly: System.Reflection.AssemblyProductAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("District 3 App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
