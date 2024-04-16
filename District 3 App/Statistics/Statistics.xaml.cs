52fad725ffde24a585583834b43204b8e2c46d26
diahl <herta.diana.5@gmail.com>
2024-04-16 17:00:17 +0300

    besties and streaks lined up

--- District 3 App/Statistics/Statistics.xaml.cs
+++ District 3 App/Statistics/Statistics.xaml.cs
@@ -20,12 +20,22 @@ namespace District_3_App.Statistics
     /// </summary>
     public partial class Statistics : UserControl
     {
-        StatisticsService srv=new StatisticsService();
+        StatisticsService srv = new StatisticsService();
         public Statistics()
         {
             InitializeComponent();
-            this.Streak1.Text =srv.getFriendName();
+            this.Bestie1.Text = srv.GetFriendNames()[0];
+            this.Bestie2.Text = srv.GetFriendNames()[1];
+            this.Bestie3.Text = srv.GetFriendNames()[2];
+            this.Bestie4.Text = srv.GetFriendNames()[3];
+            this.Bestie5.Text = srv.GetFriendNames()[4];
+
+            this.Streak1.Text = srv.GetUserStreaks()[0].ToString();
+            this.Streak2.Text = srv.GetUserStreaks()[1].ToString();
+            this.Streak3.Text = srv.GetUserStreaks()[2].ToString();
+            this.Streak4.Text = srv.GetUserStreaks()[3].ToString();
+            this.Streak5.Text = srv.GetUserStreaks()[4].ToString();
         }
-        
+
     }
 }
--- District 3 App/Statistics/StatisticsService.cs
+++ District 3 App/Statistics/StatisticsService.cs
@@ -10,15 +10,36 @@ namespace District_3_App.Statistics
     class StatisticsService
     {
         int timeSpentOnApp;
-        User friend;
-        int streakNb;
-        public StatisticsService() { }
+        Dictionary<User, int> friends = new Dictionary<User, int>();
 
-        public StatisticsService(int timeSpentOnApp, User friend, int streakNb)
+        public StatisticsService()
+        {
+
+            User user1 = new User(Guid.NewGuid(), "username1", "password1", "user1@yahoo.ro", new DateTime(2020, 4, 21, 19, 20, 29));
+            User user2 = new User(Guid.NewGuid(), "username2", "password2", "username2@gmail.ro", new DateTime(2012, 3, 22, 21, 10, 11));
+            User user3 = new User(Guid.NewGuid(), "username3", "password3", "user3@yahoo.com", new DateTime(2021, 4, 5, 23, 32, 58));
+            User user4 = new User(Guid.NewGuid(), "username4", "password4", "username4@stud.ubbcluj.ro", new DateTime(2022, 8, 30, 21, 28, 39));
+            User user5 = new User(Guid.NewGuid(), "username5", "password5", "username4@gmail.es", new DateTime(2023, 11, 22, 13, 44, 55));
+            User user6 = new User(Guid.NewGuid(), "username6", "password6", "username6@stud.ubbcluj.ro", new DateTime(2022, 8, 30, 21, 28, 39));
+            User user7 = new User(Guid.NewGuid(), "username7", "password7", "username7@gmail.es", new DateTime(2023, 11, 22, 13, 44, 55));
+            friends.Add(user1, 45);
+            friends.Add(user2, 60);
+            friends.Add(user3, 12);
+            friends.Add(user4, 30);
+            friends.Add(user5, 120);
+            friends.Add(user6, 1);
+            friends.Add(user7, 33);
+            var sortedSequence = friends.OrderByDescending(pair => pair.Value);
+            Dictionary<User, int> sortedDictionary = sortedSequence.ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            friends = sortedDictionary;
+
+        }
+
+        public StatisticsService(int timeSpentOnApp, Dictionary<User, int> friends)
         {
             this.timeSpentOnApp = timeSpentOnApp;
-            this.streakNb = streakNb;
-            this.friend = friend;
+            this.friends = friends;
         }
 
         public int seeAverageTimeSpent()
@@ -26,14 +47,43 @@ namespace District_3_App.Statistics
             return 1;
         }
 
-        public string getFriendName()
+
+
+        public List<string> GetFriendNames()
         {
-            return "@newusername";
+            List<string> friendNames = new List<string>();
+            int contor = 5;
+            foreach (User user in friends.Keys)
+            {
+                contor--;
+                friendNames.Add(user.username);
+                if (contor == 0)
+                    return friendNames;
+            }
+
+            return friendNames;
         }
+        public List<int> GetUserStreaks()
+        {
+            List<int> friendStreaks = new List<int>();
+            int contor = 5;
+            foreach (User user in friends.Keys)
+            {
+                contor--;
+                friendStreaks.Add(friends[user]);
+                if (contor == 0)
+                    return friendStreaks;
+            }
+
+            return friendStreaks;
+
+        }
+
         //TO DO:
         /*
-         * Create a list of users
+         * A more efficient method to select top 5 values from dictionary
          * Create a function that checks the consecutivity of texts 
+         * Compute the time the app runs
          */
 
     }
